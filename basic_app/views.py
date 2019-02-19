794d0979cc67ffde71b181eb0db1ab4ed76f6752
Kim Joseph <kimwrjoseph@gmail.com>
2019-02-19 16:04:57 -0500

    first

---
+++ basic_app/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.views.generic import View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from . import models
+from django.urls import reverse_lazy
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in the publisher
+        context['publisher'] = 'test'
+        return context
+
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
+
+class SchoolCreateView(CreateView):
+    fields = ['name', 'principal', 'location']
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ['name', 'principal']
+    model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+
+    model = models.School
+
+    success_url = reverse_lazy('basic_app:list')
